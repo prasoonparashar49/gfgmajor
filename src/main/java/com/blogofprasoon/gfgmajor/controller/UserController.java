01129ed2c4dd43486e540de962df86bdd1fa38c9
Prasoon <prasoonparashar49@gmail.com>
2019-06-29 10:04:52 +0530

    adding method signature

--- src/main/java/com/blogofprasoon/gfgmajor/controller/UserController.java
+++ src/main/java/com/blogofprasoon/gfgmajor/controller/UserController.java
@@ -1,6 +1,13 @@
 package com.blogofprasoon.gfgmajor.controller;
 
+import com.blogofprasoon.gfgmajor.model.User;
+
 public class UserController {
+
+    public User getUserInfo(String handle) {
+        //read data from the codeforces for the given handle
+        return null;
+    }
 
 
 }
