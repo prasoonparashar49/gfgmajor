aaaebb9b7e28f792eab17fd87b9df6e2fa8e6305
Prasoon <prasoonparashar49@gmail.com>
2019-06-29 10:15:04 +0530

    need to add new service, hence pushing changes in controller

--- src/main/java/com/blogofprasoon/gfgmajor/controller/UserController.java
+++ src/main/java/com/blogofprasoon/gfgmajor/controller/UserController.java
@@ -3,9 +3,12 @@ package com.blogofprasoon.gfgmajor.controller;
 import com.blogofprasoon.gfgmajor.model.User;
 
 public class UserController {
+    private static String url = "http://codeforces.com/api/user.info?handles=";
 
     public User getUserInfo(String handle) {
         //read data from the codeforces for the given handle
+        String handleUrl = url + handle;
+
         return null;
     }
 
